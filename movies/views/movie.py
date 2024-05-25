46f5f39a8fcc5420be396f6cdb9d775a312a9761
Carlos Zanella <cefzanella@gmail.com>
2024-05-25 18:12:33 -0300

    feat: adds filtering to MovieViewSet

--- movies/views/movie.py
+++ movies/views/movie.py
@@ -1,11 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from movies.models import Movie
 from movies.serializers import MovieSerializer, ListMovieSerializer
 
 class MovieViewSet(viewsets.ModelViewSet):
-    def get_queryset(self):
-        queryset = Movie.objects.prefetch_related('characters', 'characters__person')
-        return queryset
+    queryset = Movie.objects.prefetch_related('characters', 'characters__person')
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['primary_title']
 
     def get_serializer_class(self):
         if self.action == 'list':
