22923ddd77474e358125c94f129371bb224893e9
Carlos Zanella <cefzanella@gmail.com>
2024-05-26 18:34:53 -0300

    feat: improvements on load_credits so it continues where it stopped

--- movies/management/commands/load_credits.py
+++ movies/management/commands/load_credits.py
@@ -36,12 +36,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(f'Loading from {options['data_file']}')
+        max_tconst_from_crewmember = CrewMember.objects.last().movie.tconst if CrewMember.objects.exists() else ''
+        max_tconst_from_character = Character.objects.last().movie.tconst if Character.objects.exists() else ''
+        max_tconst = max(max_tconst_from_crewmember, max_tconst_from_character) or None
+
         start = datetime.now()
         new_credits = defaultdict(lambda:[])
         count = 0
 
         self.stdout.write('Loading movies...')
-        movies = {movie.tconst:movie for movie in Movie.objects.all()}
+        movie_queryset = Movie.objects.all()
+        if max_tconst:
+            movie_queryset = movie_queryset.filter(tconst__gt=max_tconst)
+        movies = {movie.tconst:movie for movie in movie_queryset}
         self.stdout.write(f'done {datetime.now() - start}')
 
         self.stdout.write('Loading people...')
@@ -50,6 +57,8 @@ class Command(BaseCommand):
 
         for row in islice(open(options['data_file']), 1, None):
             tconst, ordering, nconst, category, job, characters = row.split('\t')[:6]
+            if max_tconst and tconst <= max_tconst:
+                continue
             movie = movies.get(tconst, None)
             person = people.get(nconst, None)
             if movie and person:
