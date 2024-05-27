659a010cf203bab45a7cca49dea899a82ace6e83
Carlos Zanella <cefzanella@gmail.com>
2024-05-27 16:45:41 -0300

    fix: correct search fields in Movie

--- movies/views/movie.py
+++ movies/views/movie.py
@@ -5,7 +5,7 @@ from movies.serializers import MovieSerializer, ListMovieSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.prefetch_related('characters', 'characters__person')
     filter_backends = [filters.SearchFilter]
-    search_fields = ['primary_title']
+    search_fields = ['primary_title', 'original_title']
 
     def get_serializer_class(self):
         if self.action == 'list':
